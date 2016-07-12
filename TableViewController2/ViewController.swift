cee9733e1fb66eab6957229ee2419afb6ef85921
Mac Mini <macmini@macminipro-33.local>
2016-07-12 12:11:20 +0530

    Initial Commit

---
+++ TableViewController2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewController2
+//
+//  Created by Mac Mini on 12/07/16.
+//  Copyright © 2016 Kumar Mote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
